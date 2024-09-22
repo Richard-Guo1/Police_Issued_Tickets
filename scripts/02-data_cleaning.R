d2b48113d8ee3aebba15d01047a35cffa2ce72e2
Richard Guo <richard.guo@mail.utoronto.ca>
2024-09-22 20:34:29 +0000

    create skeleton of paper

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -22,10 +22,10 @@ raw_data <- read_csv("data/raw_data/raw_data.csv")
 # Remove unusable data
 cleaned_data <- subset(raw_data, DIVISION != "NSA" & HOOD_158 != "NSA")
 
-### Remove non-relevant information ###
+# Remove non-relevant information
 cleaned_data <- cleaned_data %>% select(-c(X_id,TICKET_TYPE,NEIGHBOURHOOD_158))
 
-### Rename column names ###
+# Rename column names
 names(cleaned_data) <- c("Offence Year", "Division", "Offence Category", "Age 
                          Group", "District Number", "Ticket Count")
 
