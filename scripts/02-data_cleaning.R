3c1687179710f57bbc50c51ba7367405336fceb6
Richard Guo <richard.guo@mail.utoronto.ca>
2024-09-22 18:09:13 +0000

    clean data by removing all rows with unusable data

---
+++ scripts/02-data_cleaning.R
@@ -0,0 +1,21 @@
+#### Preamble ####
+# Purpose: Cleans the raw ticket data into an analysis dataset
+# Author: Richard Guo
+# Date: 21 September 2024
+# Contact: richard.guo@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Need to have downloaded the data
+# Any other information needed? None.
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Clean data ####
+raw_data <- read_csv("data/raw_data/raw_data.csv")
+
+#### Remove unusable data ###
+cleaned_data <- subset(raw_data, DIVISION != "NSA" & HOOD_158 != "NSA")
+
+
+#### Save data ####
+write_csv(cleaned_data, "data/analysis_data/analysis_data.csv")
